4a308f1154e5d851dcde20ff778a9c47de217cb7
You Matsumoto <youmts22@gmail.com>
2019-09-16 16:58:43 +0900

    不要なroutesを削除

--- config/routes.rb
+++ config/routes.rb
@@ -12,8 +12,6 @@ Rails.application.routes.draw do
 
   devise_for :admins, controllers: {
     sessions: 'admins/sessions',
-    passwords: 'admins/passwords',
-    registrations: 'admins/registrations',
   }
 
   devise_for :users, controllers: {
